a1c9f22ca77bc94bb99701d04bc135a720ef2b3c
findecano <jacruzjacruz@gmail.com>
2024-06-09 23:18:21 -0500

    Limitar rutas, solo Churches nearby y by_id

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+/*
 app.use('/api/v1', require('./src/routes/domainRoutes'));
 app.use('/api/v1', require('./src/routes/domainValueRoutes'));
 app.use('/api/v1', require('./src/routes/locationRoutes'));
 app.use('/api/v1', require('./src/routes/userRoutes'));
 app.use('/api/v1', require('./src/routes/linkRoutes'));
-app.use('/api/v1', require('./src/routes/scheduleRoutes'));
+app.use('/api/v1', require('./src/routes/scheduleRoutes'));*/
 app.use('/api/v1', require('./src/routes/churchRoutes'));
 
 app.listen(port, ()=>{
--- src/routes/churchRoutes.js
+++ src/routes/churchRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const churches = require('../controllers/churchController');
 
 router.get('/churches-nearby/', churches.getChurchesNearby);
-router.get('/churches/', churches.getAllChurches);
 router.get('/churches/:id', churches.getOneChurch);
+/*router.get('/churches/', churches.getAllChurches);
 router.post('/churches', churches.createChurch);
 router.put('/churches/:id', churches.updateChurch);
 router.delete('/churches/:id', churches.deleteChurch);
-
+*/
 module.exports = router;
